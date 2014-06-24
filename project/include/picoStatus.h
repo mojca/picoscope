b2659c7dc480989cf464114e6a082284b6400d2a
Mojca Miklavec <mojca.miklavec@gmail.com>
2014-06-24 02:00:08 +0200

    update picoStatus

--- project/include/picoStatus.h
+++ project/include/picoStatus.h
@@ -1,7 +1,7 @@
 /****************************************************************************
  *
  * Filename:    pico_status.h
- * Copyright:   Pico Technology Limited 2002 - 2006
+ * Copyright:   Pico Technology Limited 2002 - 2013
  * Author:      MAS
  * Description:
  *
@@ -12,6 +12,8 @@
 #ifndef __PICOSTATUS_H__
 #define __PICOSTATUS_H__
 
+#include <stdint.h>
+
 #define PICO_DRIVER_VERSION                       0x00000000UL
 #define PICO_USB_VERSION                          0x00000001UL
 #define PICO_HARDWARE_VERSION                     0x00000002UL
@@ -20,7 +22,15 @@
 #define PICO_CAL_DATE                             0x00000005UL
 #define PICO_KERNEL_VERSION                       0x00000006UL
 
-typedef unsigned long PICO_INFO;
+#define PICO_DIGITAL_HARDWARE_VERSION             0x00000007UL
+#define PICO_ANALOGUE_HARDWARE_VERSION            0x00000008UL
+
+#define PICO_FIRMWARE_VERSION_1                   0x00000009UL
+#define PICO_FIRMWARE_VERSION_2                   0x0000000AUL
+
+#define PICO_MAC_ADDRESS                          0x0000000BUL
+
+typedef uint32_t PICO_INFO;
 
 #define PICO_OK                                   0x00000000UL
 #define PICO_MAX_UNITS_OPENED                     0x00000001UL
@@ -100,22 +110,108 @@ typedef unsigned long PICO_INFO;
 #define PICO_RATIO_MODE_NOT_SUPPORTED             0x00000047UL
 #define PICO_RAPID_NOT_SUPPORT_AGGREGATION        0x00000048UL
 #define PICO_INVALID_TRIGGER_PROPERTY             0x00000049UL
-// added manually
 #define PICO_INTERFACE_NOT_CONNECTED              0x0000004AUL
+#define PICO_RESISTANCE_AND_PROBE_NOT_ALLOWED     0x0000004BUL
+#define PICO_POWER_FAILED                         0x0000004CUL
 #define PICO_SIGGEN_WAVEFORM_SETUP_FAILED         0x0000004DUL
 #define PICO_FPGA_FAIL                            0x0000004EUL
 #define PICO_POWER_MANAGER                        0x0000004FUL
 #define PICO_INVALID_ANALOGUE_OFFSET              0x00000050UL
+// unable to configure the ps6000
 #define PICO_PLL_LOCK_FAILED                      0x00000051UL
+// the ps6000 Analog board is not detectly connected
+//to the digital board
 #define PICO_ANALOG_BOARD                         0x00000052UL
+// unable to configure the Signal Generator
 #define PICO_CONFIG_FAIL_AWG                      0x00000053UL
-#define PICO_INITIALIZE_FPGA                      0x00000054UL
+#define PICO_INITIALISE_FPGA                      0x00000054UL
 #define PICO_EXTERNAL_FREQUENCY_INVALID           0x00000056UL
 #define PICO_CLOCK_CHANGE_ERROR                   0x00000057UL
 #define PICO_TRIGGER_AND_EXTERNAL_CLOCK_CLASH     0x00000058UL
 #define PICO_PWQ_AND_EXTERNAL_CLOCK_CLASH         0x00000059UL
+#define PICO_UNABLE_TO_OPEN_SCALING_FILE          0x0000005AUL
+
+#define PICO_MEMORY_CLOCK_FREQUENCY               0x0000005BUL
+#define PICO_I2C_NOT_RESPONDING                   0x0000005CUL
+
+#define PICO_NO_CAPTURES_AVAILABLE                0x0000005DUL
+#define PICO_NOT_USED_IN_THIS_CAPTURE_MODE        0x0000005EUL
+
 #define PICO_GET_DATA_ACTIVE                      0x00000103UL
 
-typedef unsigned long PICO_STATUS;
+// used by the PT104 (USB) when connected via the Network Socket
+#define PICO_IP_NETWORKED                                0x00000104UL
+#define PICO_INVALID_IP_ADDRESS                          0x00000105UL
+#define PICO_IPSOCKET_FAILED                             0x00000106UL
+#define PICO_IPSOCKET_TIMEDOUT                           0x00000107UL
+#define PICO_SETTINGS_FAILED                             0x00000108UL
+#define PICO_NETWORK_FAILED                              0x00000109UL
+#define PICO_WS2_32_DLL_NOT_LOADED                       0x0000010AUL
+#define PICO_INVALID_IP_PORT                             0x0000010BUL
+
+#define PICO_COUPLING_NOT_SUPPORTED                      0x0000010CUL
+#define PICO_BANDWIDTH_NOT_SUPPORTED                     0x0000010DUL
+#define PICO_INVALID_BANDWIDTH                           0x0000010EUL
+
+#define PICO_AWG_NOT_SUPPORTED                           0x0000010FUL
+#define PICO_ETS_NOT_RUNNING                             0x00000110UL
+#define PICO_SIG_GEN_WHITENOISE_NOT_SUPPORTED            0x00000111UL
+#define PICO_SIG_GEN_WAVETYPE_NOT_SUPPORTED              0x00000112UL
+
+#define PICO_INVALID_DIGITAL_PORT                        0x00000113UL
+#define PICO_INVALID_DIGITAL_CHANNEL                     0x00000114UL
+#define PICO_INVALID_DIGITAL_TRIGGER_DIRECTION           0x00000115UL
+
+#define PICO_SIG_GEN_PRBS_NOT_SUPPORTED                  0x00000116UL
+
+#define PICO_ETS_NOT_AVAILABLE_WITH_LOGIC_CHANNELS       0x00000117UL
+
+#define PICO_WARNING_REPEAT_VALUE                        0x00000118UL
+
+#define PICO_POWER_SUPPLY_CONNECTED                      0x00000119UL
+#define PICO_POWER_SUPPLY_NOT_CONNECTED                  0x0000011AUL
+#define PICO_POWER_SUPPLY_REQUEST_INVALID                0x0000011BUL
+#define PICO_POWER_SUPPLY_UNDERVOLTAGE                   0x0000011CUL
+
+#define PICO_CAPTURING_DATA                              0x0000011DUL
+
+#define PICO_USB3_0_DEVICE_NON_USB3_0_PORT               0x0000011EUL
+
+#define PICO_NOT_SUPPORTED_BY_THIS_DEVICE                0x0000011FUL
+#define PICO_INVALID_DEVICE_RESOLUTION                   0x00000120UL
+#define PICO_INVALID_NUMBER_CHANNELS_FOR_RESOLUTION      0x00000121UL
+
+#define PICO_CHANNEL_DISABLED_DUE_TO_USB_POWERED         0x00000122UL
+
+#define PICO_SIGGEN_DC_VOLTAGE_NOT_CONFIGURABLE          0x00000123UL
+
+#define PICO_NO_TRIGGER_ENABLED_FOR_TRIGGER_IN_PRE_TRIG  0x00000124UL
+#define PICO_TRIGGER_WITHIN_PRE_TRIG_NOT_ARMED           0x00000125UL
+#define PICO_TRIGGER_WITHIN_PRE_NOT_ALLOWED_WITH_DELAY   0x00000126UL
+#define PICO_TRIGGER_INDEX_UNAVAILABLE                   0x00000127UL
+
+#define PICO_AWG_CLOCK_FREQUENCY                         0x00000128UL
+// there are more than 4 analogue channels with a trigger condition set
+#define PICO_TOO_MANY_CHANNELS_IN_USE                    0x00000129UL
+
+#define PICO_NULL_CONDITIONS                             0x000012AUL
+#define PICO_DUPLICATE_CONDITION_SOURCE                  0x0000012BUL
+#define PICO_INVALID_CONDITION_INFO                      0x0000012CUL
+
+#define PICO_SETTINGS_READ_FAILED                        0x0000012DUL
+#define PICO_SETTINGS_WRITE_FAILED                       0x0000012EUL
+
+#define PICO_ARGUMENT_OUT_OF_RANGE                       0x0000012FUL
+
+#define PICO_HARDWARE_VERSION_NOT_SUPPORTED              0x00000130UL
+#define PICO_DIGITAL_HARDWARE_VERSION_NOT_SUPPORTED      0x00000131UL
+#define PICO_ANALOGUE_HARDWARE_VERSION_NOT_SUPPORTED     0x00000132UL
+
+#define PICO_DEVICE_TIME_STAMP_RESET                     0x01000000UL
+
+#define PICO_WATCHDOGTIMER                               0x10000000UL
+
+typedef uint32_t PICO_STATUS;
+
 
 #endif
