7590b35a7d185be46134c0d5a907778b2ac2baea
Mojca Miklavec <mojca.miklavec@gmail.com>
2014-06-24 17:00:29 +0200

    a very simple CMake build file

---
+++ project/toolchain-i386-mingw32.cmake
@@ -0,0 +1,21 @@
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Windows)
+
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER i386-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER i386-mingw32-g++)
+SET(CMAKE_RC_COMPILER i386-mingw32-windres)
+
+SET(CMAKE_EXECUTABLE_SUFFIX .exe)
+
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH /opt/local/i386-mingw32)
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search 
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# cmake -DCMAKE_TOOLCHAIN_FILE=toolchain-i386-mingw32.cmake -DCMAKE_INSTALL_PREFIX=...
